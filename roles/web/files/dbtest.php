cc9ee457b4248bb545a6cfd82d77dd6a112b5af8
s1118806 <39749550+s1118806@users.noreply.github.com>
2018-05-30 02:39:04 +0200

    Add files via upload

---
+++ roles/web/files/dbtest.php
@@ -0,0 +1,29 @@
+<h1>mysqli</h1>
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+<h1>PDO</h1>
+ <?php
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=mysql", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>  
